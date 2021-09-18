950a226a8ff0420a9896156bd1a294ac018520e5
Sachin Hiriyanna <gvarchana95@gmail.com>
2021-09-18 19:32:13 -0400

    Added Router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { Route, Switch } from "react-router-dom";
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupPage from "./pages/NewMeetups";
+import Favoritespage from "./pages/Favorites";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Switch>
+        <Route path="/" exact>
+          <AllMeetupsPage />
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetupPage />
+        </Route>
+        <Route path="/favorites">
+          <Favoritespage />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
