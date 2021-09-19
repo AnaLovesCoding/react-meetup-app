8c0630c4761d9c306d2f2de22b086b0099013e17
Sachin Hiriyanna <gvarchana95@gmail.com>
2021-09-19 15:37:31 -0400

    Imported useRef from react

--- src/components/layout/meetups/NewMeetupForm.js
+++ src/components/layout/meetups/NewMeetupForm.js
@@ -1,25 +1,46 @@
 import Card from "../../ui/Card";
 import classes from "./NewMeetupForm.module.css";
+import { useRef } from "react";
 
 function NewMeetupForm() {
+  const titleInputref = useRef();
+  const imageInputref = useRef();
+  const addressInputref = useRef();
+  const descrptionInputref = useRef();
+
+  function submitHandler(event) {
+    event.preventDefault();
+    const enteredTitle = titleInputref.current.value;
+    const enteredImage = imageInputref.current.value;
+    const enteredAddress = addressInputref.current.value;
+    const enteredDescrption = descrptionInputref.current.value;
+
+    const meetupData = {
+      title: enteredTitle,
+      image: enteredImage,
+      address: enteredAddress,
+      description: enteredDescrption,
+    };
+    console.log(meetupData);
+  }
   return (
     <Card>
-      <form className={classes.form}>
+      <form className={classes.form} onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor="title">Meetup title</label>
-          <input type="text" required id="title" />
+          <input type="text" required id="title" ref={titleInputref} />
         </div>
         <div className={classes.control}>
           <label htmlFor="image">Meetup Image</label>
-          <input type="url" required id="image" />
+          <input type="url" required id="image" ref={imageInputref} />
         </div>
         <div className={classes.control}>
           <label htmlFor="address">Address</label>
-          <input type="text" required id="address" />
+          <input type="text" required id="address" ref={addressInputref} />
         </div>
         <div className={classes.control}>
           <label htmlFor="desscription">Descrption</label>
-          <input type="text" required row="5" />
+          <input type="text" required row="5" ref={descrptionInputref} />
         </div>
         <div className={classes.actions}>
           <button>Add Meetup</button>
